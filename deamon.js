1037b202a756dd0189920e8b5d1386129d4e744e
ZhangZisu <zhangzisu@outlook.com>
2018-05-31 08:04:40 +0800

    Upd

--- deamon.js
+++ deamon.js
@@ -3,11 +3,13 @@ let child_process = Promise.promisifyAll(require('child_process'));
 
 async function run() {
     let output;
+    console.log('Service started.');
     try {
         output = await child_process.execAsync('npm start');
     } catch (e) {
         output = e.toString();
     }
+    console.log(output);
     setTimeout(run, 10000);
 }
 
